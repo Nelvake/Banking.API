5892cb690e69d2aac72b979134ef7ca665e1b97f
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-14 20:31:45 +0600

    feat <DataAccess.Interfaces> : Добавлены интерфейсы для ORM.

---
+++ Banking/Banking.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.DataAccess.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        bool Add(T entity);
+
+        bool Edit(T entity);
+
+        bool Delete(T entity);
+
+        T Get(Guid id);
+
+        IList<T> GetAll();
+    }
+}
---
+++ Banking/Banking.DataAccess/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using Banking.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.DataAccess.Interfaces
+{
+    public interface IUnitOfWork
+    {
+        IRepository<BankAccount> BankAccounts { get; set; }
+        IRepository<BankCard> BankCards { get; set; }
+        IRepository<User> Users { get; set; }
+        IRepository<Service> Services { get; set; }
+        IRepository<ServiceProvider> ServiceProviders { get; set; }
+
+        void Save();
+
+        void BeginTransaction();
+
+        void Commit();
+
+        void Rollback();
+    }
+}
