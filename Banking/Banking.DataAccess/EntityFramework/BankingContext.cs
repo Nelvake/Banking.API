6454822ac77f8a08b6e4f71e91adf1c88ee2a3a6
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-14 20:32:42 +0600

    feat <EF> : Добавлен EF.

---
+++ Banking/Banking.DataAccess/EntityFramework/BankingContext.cs
@@ -0,0 +1,22 @@
+﻿using Banking.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.DataAccess.EntityFramework
+{
+    public class BankingContext : DbContext
+    {
+        public DbSet<BankAccount> BankAccounts { get; set; }
+        public DbSet<BankCard> BankCards { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Service> Services { get; set; }
+        public DbSet<ServiceProvider> ServiceProviders { get; set; }
+
+        public BankingContext(DbContextOptions<BankingContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
