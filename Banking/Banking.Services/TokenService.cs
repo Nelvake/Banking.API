dedca8c8bbddcdb4e86bbe82d26a54c686745b4a
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-15 17:44:20 +0600

    chore <TokenService> : Метод CreateAuthToken обернут в блок try/catch.

--- Banking/Banking.Services/TokenService.cs
+++ Banking/Banking.Services/TokenService.cs
@@ -1,5 +1,6 @@
 ﻿using Banking.Domain.Options;
 using Banking.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using System;
@@ -13,26 +14,36 @@ namespace Banking.Services
     public class TokenService : ITokenService
     {
         private readonly IOptions<AppSettingsOptions> _appOptions;
+        private readonly ILogger<TokenService> _logger;
 
-        public TokenService(IOptions<AppSettingsOptions> appOptions)
+        public TokenService(IOptions<AppSettingsOptions> appOptions, ILogger<TokenService> logger)
         {
             _appOptions = appOptions;
+            _logger = logger;
         }
 
         public string CreateAuthToken(ClaimsIdentity claimsIdentity)
         {
-            var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appOptions.Value.Secret));
-            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            try
+            {
+                var key = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_appOptions.Value.Secret));
+                var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            var token = new JwtSecurityToken(
-                _appOptions.Value.Issuer,
-                _appOptions.Value.Audience,
-                notBefore: DateTime.Now,
-                claims: claimsIdentity.Claims,
-                expires: DateTime.Now.AddYears(50),
-                signingCredentials: signingCredentials);
+                var token = new JwtSecurityToken(
+                    _appOptions.Value.Issuer,
+                    _appOptions.Value.Audience,
+                    notBefore: DateTime.Now,
+                    claims: claimsIdentity.Claims,
+                    expires: DateTime.Now.AddYears(50),
+                    signingCredentials: signingCredentials);
 
-            return new JwtSecurityTokenHandler().WriteToken(token);
+                return new JwtSecurityTokenHandler().WriteToken(token);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                throw;
+            }
         }
     }
 }
