0c4639a64ba445448aa0fb01d145891be9956e84
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-15 16:57:57 +0600

    feat <AuthDTO> : Добавлено DTO для авторизации.

---
+++ Banking/Banking.DTOs/AuthDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.DTOs
+{
+    public class AuthDTO
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
