024bb621334501504bd9946f0cf42fc2c8a1f42c
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-16 17:37:09 +0600

    feat <BankAccountRepository> : Добавлена имплементация для репозитория BankAccount.

--- Banking/Banking.DataAccess/EntityFramework/EFImplementaions/BankAccountRepository.cs
+++ Banking/Banking.DataAccess/EntityFramework/EFImplementaions/BankAccountRepository.cs
@@ -1,36 +1,77 @@
 ﻿using Banking.DataAccess.Interfaces;
 using Banking.Domain;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Banking.DataAccess.EntityFramework.EFImplementaions
 {
     public class BankAccountRepository : IRepository<BankAccount>
     {
+        private readonly BankingContext _context;
+        private readonly ILogger<BankAccountRepository> _logger;
+
+        public BankAccountRepository(BankingContext context, ILogger<BankAccountRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
         public bool Add(BankAccount entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.BankAccounts.Add(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public bool Delete(BankAccount entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.BankAccounts.Remove(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public bool Edit(BankAccount entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.BankAccounts.Update(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public BankAccount Get(Guid id)
         {
-            throw new NotImplementedException();
+            return _context.BankAccounts.FirstOrDefault(x => x.Id == id);
         }
 
         public IList<BankAccount> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.BankAccounts.ToList();
         }
     }
 }
