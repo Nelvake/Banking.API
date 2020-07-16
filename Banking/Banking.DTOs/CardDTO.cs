ffcaf7db952e7754b43d6cb6b037e669b375ae80
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-16 18:57:02 +0600

    chore <CardDTO> : Добавлено DTO для CardController.

---
+++ Banking/Banking.DTOs/CardDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.DTOs
+{
+    public class CardDTO
+    {
+        public string CardHolder { get; set; }
+    }
+}
