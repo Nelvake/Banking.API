38cf64910806ecee64d4617b7288aa8210c3d447
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-16 19:33:53 +0600

    chore <BankCard> : Добавлена инициализация поля ValidDate.

--- Banking/Banking.Domain/BankCard.cs
+++ Banking/Banking.Domain/BankCard.cs
@@ -6,7 +6,7 @@ namespace Banking.Domain
     public class BankCard : Entity
     {
         public string CardNumber { get; set; }
-        public DateTime ValidDate { get; set; }
+        public DateTime ValidDate { get; set; } = DateTime.Now.AddYears(2);
         public string CardHolder { get; set; }
         public int CVC { get; set; }
         public decimal Amount { get; set; }
