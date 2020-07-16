212d33082ae130d9f1dd1d2308c7f737e9d28050
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-16 18:50:50 +0600

    feat <BankCardRepository> : Добавлена имплементация для репозитория BankCard.

--- Banking/Banking.DataAccess/EntityFramework/EFImplementaions/BankCardRepository.cs
+++ Banking/Banking.DataAccess/EntityFramework/EFImplementaions/BankCardRepository.cs
@@ -1,36 +1,78 @@
 ﻿using Banking.DataAccess.Interfaces;
 using Banking.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Banking.DataAccess.EntityFramework.EFImplementaions
 {
     public class BankCardRepository : IRepository<BankCard>
     {
+        private readonly BankingContext _context;
+        private readonly ILogger<BankAccountRepository> _logger;
+
+        public BankCardRepository(BankingContext context, ILogger<BankAccountRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
         public bool Add(BankCard entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.BankCards.Add(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public bool Delete(BankCard entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.BankCards.Remove(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public bool Edit(BankCard entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.BankCards.Update(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public BankCard Get(Guid id)
         {
-            throw new NotImplementedException();
+            return _context.BankCards.Include(x => x.BankAccount).FirstOrDefault(x => x.Id == id);
         }
 
         public IList<BankCard> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.BankCards.Include(x => x.BankAccount).ToList();
         }
     }
 }
