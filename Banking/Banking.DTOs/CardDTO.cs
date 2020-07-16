368ffb98f047b58429576b873c3250ac25d86ee3
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-16 19:33:20 +0600

    chore <CardDTO> : Изменен DTO.

--- Banking/Banking.DTOs/CardDTO.cs
+++ Banking/Banking.DTOs/CardDTO.cs
@@ -6,6 +6,10 @@ namespace Banking.DTOs
 {
     public class CardDTO
     {
+        public string CardNumber { get; set; }
+        public DateTime ValidDate { get; set; }
         public string CardHolder { get; set; }
+        public int CVC { get; set; }
+        public decimal Amount { get; set; }
     }
 }
