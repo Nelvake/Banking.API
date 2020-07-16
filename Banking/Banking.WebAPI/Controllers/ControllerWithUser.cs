7e2cff96410d0cc3d54c611f41c6b5060d117266
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-16 19:38:57 +0600

    feat <ControllerWithUser> : Добавлен абстрактный контроллер в котором есть текущий авторизованный пользователь.

---
+++ Banking/Banking.WebAPI/Controllers/ControllerWithUser.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Banking.WebAPI.Controllers
+{
+    public abstract class ControllerWithUser : ControllerBase
+    {
+        private IEnumerable<Claim> _userClaims;
+
+        public IEnumerable<Claim> UserClaims
+        {
+            get => _userClaims ?? User.Claims;
+            set => _userClaims = value;
+        }
+
+        protected Guid UserId
+        {
+            get
+            {
+                Guid.TryParse(UserClaims.FirstOrDefault(claim => claim.Type == ClaimTypes.Sid)?.Value,
+                    out var userId);
+                return userId;
+            }
+        }
+    }
+}
