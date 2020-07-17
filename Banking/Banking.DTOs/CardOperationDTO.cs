f2c0d4205c96310e9b7d5e79eff6f795eb6f1e83
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-17 17:48:56 +0600

    feat <CardOperationDTO> : Добавлен DTO для CardOperation.

---
+++ Banking/Banking.DTOs/CardOperationDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking.DTOs
+{
+    public class CardOperationDTO
+    {
+        public Guid CardId { get; set; }
+        public string NumberCardForTransfer { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
