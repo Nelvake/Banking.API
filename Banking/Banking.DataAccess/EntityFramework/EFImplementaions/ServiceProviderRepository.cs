2456899f4e54b2f094f63e0d79f741c7136cb8c5
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-19 17:46:47 +0600

    feat <ServiceProviderRepository> : Добавлена имплементация.

--- Banking/Banking.DataAccess/EntityFramework/EFImplementaions/ServiceProviderRepository.cs
+++ Banking/Banking.DataAccess/EntityFramework/EFImplementaions/ServiceProviderRepository.cs
@@ -1,36 +1,77 @@
 ﻿using Banking.DataAccess.Interfaces;
 using Banking.Domain;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Banking.DataAccess.EntityFramework.EFImplementaions
 {
     public class ServiceProviderRepository : IRepository<ServiceProvider>
     {
+        private readonly BankingContext _context;
+        private readonly ILogger<ServiceProviderRepository> _logger;
+
+        public ServiceProviderRepository(BankingContext context, ILogger<ServiceProviderRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
         public bool Add(ServiceProvider entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.ServiceProviders.Add(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public bool Delete(ServiceProvider entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.ServiceProviders.Remove(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public bool Edit(ServiceProvider entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.ServiceProviders.Update(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public ServiceProvider Get(Guid id)
         {
-            throw new NotImplementedException();
+            return _context.ServiceProviders.Find(id);
         }
 
         public IList<ServiceProvider> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.ServiceProviders.ToList();
         }
     }
 }
