f98f716a235fa66f26353c94b29f80d5687bf87e
NickSagalbaev <NickSagalbaev@gmail.com>
2020-07-19 17:47:13 +0600

    feat <ServiceRepository> : Добавлена имплементация.

--- Banking/Banking.DataAccess/EntityFramework/EFImplementaions/ServiceRepository.cs
+++ Banking/Banking.DataAccess/EntityFramework/EFImplementaions/ServiceRepository.cs
@@ -1,36 +1,78 @@
 ﻿using Banking.DataAccess.Interfaces;
 using Banking.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Banking.DataAccess.EntityFramework.EFImplementaions
 {
     public class ServiceRepository : IRepository<Service>
     {
+        private readonly BankingContext _context;
+        private readonly ILogger<ServiceRepository> _logger;
+
+        public ServiceRepository(BankingContext context, ILogger<ServiceRepository> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
         public bool Add(Service entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.Services.Add(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public bool Delete(Service entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.Services.Remove(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public bool Edit(Service entity)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.Services.Update(entity);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                throw;
+            }
         }
 
         public Service Get(Guid id)
         {
-            throw new NotImplementedException();
+            return _context.Services.Include(x => x.BankCard).Include(x => x.ServiceProvider).FirstOrDefault(x => x.Id == id);
         }
 
         public IList<Service> GetAll()
         {
-            throw new NotImplementedException();
+            return _context.Services.Include(x => x.BankCard).Include(x => x.ServiceProvider).ToList();
         }
     }
 }
